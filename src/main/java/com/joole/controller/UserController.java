d42d9d9134d84bf23f7e9eba6318498cf0fbc9f5
JIANAN WEN <jiananwen@JIANANs-MacBook-Pro-2.local>
2019-04-15 14:23:05 -0400

    Structure with Product api and restful example

--- src/main/java/com/joole/controller/UserController.java
+++ src/main/java/com/joole/controller/UserController.java
@@ -2,33 +2,39 @@ package com.joole.controller;
 
 
 
-import java.io.IOException;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
+
 
 import com.joole.domain.User;
 import com.joole.service.UserService;
 
-@Controller
+@RestController
 public class UserController {
 	
 	@Autowired
 	private UserService userService;
 	
-	// user login
-	@RequestMapping(value = "/user", method=RequestMethod.GET)
-    public ModelAndView listEmployee(ModelAndView model) throws IOException {
-        User user = userService.getUser(1);
-        
-        System.out.println(user);
-
-        model.setViewName("home");
-        return model;
-    } 
+	// get user by id
+	@RequestMapping(value = "/user/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> getUser(@PathVariable("id") int id) {
+        System.out.println("Fetching User with id " + id);
+        User user = userService.getUser(id);
+        if (user == null) {
+            System.out.println("User with id " + id + " not found");
+            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
 	
 
 
